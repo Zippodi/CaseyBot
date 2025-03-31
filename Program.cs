6722ffdb2ae328c8f2d5d95b63ad63a528d708ed
Zippodi <Zippodithereal@gmail.com>
2025-03-31 19:20:24 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using CaseyBot.commands;
+using CaseyBot.config;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Extensions;
+using DSharpPlus.VoiceNext;
+using Newtonsoft.Json;
+
+namespace CaseyBot
+{
+    class Program
+    {
+        public static DiscordClient Client { get; set; }
+
+        //public static DiscordGuild Guild { get; set; }
+        public static CommandsNextExtension Commands { get; set; }
+        static async Task Main(string[] args)
+        {
+            var jsonReader = new JSONReader();
+            await jsonReader.ReadJSON();
+
+            var discordConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = jsonReader.token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+
+            Client = new DiscordClient(discordConfig);
+
+            var voiceConfig = new VoiceNextConfiguration
+            {
+                EnableIncoming = true
+            };
+            var voice = Client.UseVoiceNext(voiceConfig);
+
+            Client.UseInteractivity(new InteractivityConfiguration()
+            {
+                Timeout = TimeSpan.FromMinutes(2)
+            });
+
+            //DiscordChannel channel;
+            //VoiceNextConnection connection = await channel.ConnectAsync();
+
+            Client.Ready += Client_Ready;
+            Client.MessageCreated += MessageCreatedHandler;
+            Client.VoiceStateUpdated += VoiceChannelHandler;
+
+            var commandsConfig = new CommandsNextConfiguration()
+            {
+                StringPrefixes = new string[] { jsonReader.prefix },
+                EnableMentionPrefix = true,
+                EnableDms = true,
+                EnableDefaultHelp = false
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            Commands.RegisterCommands<BasicCommands>();
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+
+        }
+
+        private static async Task VoiceChannelHandler(DiscordClient sender, VoiceStateUpdateEventArgs e)
+        {
+            if (e.Before == null && e.Channel.Name == "General")
+            {
+                await e.Channel.SendMessageAsync($"{e.User.Username} has joined the channel!");
+            }
+        }
+
+        private static async Task MessageCreatedHandler(DiscordClient sender, MessageCreateEventArgs e)
+        {
+            if (!e.Message.Author.IsBot)
+            {
+                await e.Channel.SendMessageAsync("This event was triggered");
+
+            }
+        }
+
+        private static Task Client_Ready(DiscordClient sender, ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ commands/BasicCommands.cs
@@ -0,0 +1,187 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity.Extensions;
+using DSharpPlus.VoiceNext;
+
+namespace CaseyBot.commands
+{
+    public class BasicCommands : BaseCommandModule
+    {
+
+
+
+        [Command("join")]
+        public async Task JoinCommand(CommandContext ctx, DiscordChannel channel = null)
+        {
+            channel ??= ctx.Member.VoiceState?.Channel;
+            await channel.ConnectAsync();
+        }
+
+
+        [Command("leave")]
+        public async Task LeaveCommand(CommandContext ctx)
+        {
+            var vnext = ctx.Client.GetVoiceNext();
+            var connection = vnext.GetConnection(ctx.Guild);
+
+            connection.Disconnect();
+        }
+
+
+        [Command("test")]
+        public async Task TestCommand(CommandContext ctx)
+        {
+            var interactivity = Program.Client.GetInteractivity();
+
+            var messageToRetrieve = await interactivity.WaitForMessageAsync(message => message.Content == "Hello");
+
+            if (messageToRetrieve.Result.Content == "Hello")
+            {
+                await ctx.Channel.SendMessageAsync($"{ctx.User.Username} said Hello");
+            }
+
+        }
+
+        [Command("poll")]
+        public async Task Poll(CommandContext ctx, string option1, string option2, string option3, string option4, [RemainingText] string pollTitle)
+        {
+            var interactivity = Program.Client.GetInteractivity();
+            var pollTime = TimeSpan.FromSeconds(10);
+
+            DiscordEmoji[] emojiOptions = { DiscordEmoji.FromName(Program.Client, ":one:"),
+                                            DiscordEmoji.FromName(Program.Client, ":two:"),
+                                            DiscordEmoji.FromName(Program.Client, ":three:"),
+                                            DiscordEmoji.FromName(Program.Client, ":four:") };
+
+            string optionsDescription = $"{emojiOptions[0]} | {option1} \n" +
+                                        $"{emojiOptions[1]} | {option2} \n" +
+                                        $"{emojiOptions[2]} | {option3} \n" +
+                                        $"{emojiOptions[3]} | {option4} \n";
+
+            var pollMessage = new DiscordEmbedBuilder
+            {
+                Color = DiscordColor.Red,
+                Title = pollTitle,
+                Description = optionsDescription
+            };
+
+            var sentPoll = await ctx.Channel.SendMessageAsync(embed: pollMessage);
+            foreach (var emoji in emojiOptions)
+            {
+                await sentPoll.CreateReactionAsync(emoji);
+            }
+
+            var totalReactions = await interactivity.CollectReactionsAsync(sentPoll, pollTime);
+
+            int count1 = 0;
+            int count2 = 0;
+            int count3 = 0;
+            int count4 = 0;
+
+            foreach (var emoji in totalReactions)
+            {
+                if (emoji.Emoji == emojiOptions[0])
+                {
+                    count1++;
+                }
+                if (emoji.Emoji == emojiOptions[1])
+                {
+                    count2++;
+                }
+                if (emoji.Emoji == emojiOptions[2])
+                {
+                    count3++;
+                }
+                if (emoji.Emoji == emojiOptions[3])
+                {
+                    count4++;
+                }
+            }
+
+            int totalVotes = count1 + count2 + count3 + count4;
+            string resultsDescription = $"{emojiOptions[0]}: {count1} Votes \n" +
+                                        $"{emojiOptions[1]}: {count2} Votes \n" +
+                                        $"{emojiOptions[2]}: {count3} Votes \n" +
+                                        $"{emojiOptions[3]}: {count4} Votes \n\n" +
+                                        $"Total Votes: {totalVotes}";
+
+            var resultEmbed = new DiscordEmbedBuilder
+            {
+                Color = DiscordColor.Green,
+                Title = "Results of the Poll",
+                Description = resultsDescription
+            };
+
+            await ctx.Channel.SendMessageAsync(embed: resultEmbed);
+        }
+
+        [Command("add")]
+        public async Task Add(CommandContext ctx, int number1, int number2)
+        {
+            await ctx.Channel.SendMessageAsync($"Answer is {number1 + number2}");
+        }
+
+        [Command("embed")]
+        public async Task EmbedMessage(CommandContext ctx)
+        {
+            var message = new DiscordEmbedBuilder()
+            {
+                Title = "This is my first Discord Embed",
+                Description = $"This command was executed by {ctx.User.Username}",
+                Color = DiscordColor.Blue
+            };
+
+
+            await ctx.Channel.SendMessageAsync(embed: message);
+        }
+
+        //[Command("cardgame")]
+        //public async Task CardGame(CommandContext ctx)
+        //{
+        //    var userCard = new CardSystem();
+
+        //    var userCardEmbed = new DiscordEmbedBuilder
+        //    {
+        //        Title = $"Your Card is {userCard.SelectedCard}",
+        //        Color = DiscordColor.Orange
+        //    };
+
+        //    await ctx.Channel.SendMessageAsync(embed: userCardEmbed);
+
+        //    var botCard = new CardSystem();
+
+        //    var botCardEmbed = new DiscordEmbedBuilder
+        //    {
+        //        Title = $"The Bot drew a {botCard.SelectedCard}",
+        //        Color = DiscordColor.Purple
+        //    };
+
+        //    await ctx.Channel.SendMessageAsync(embed: botCardEmbed);
+
+        //    if (userCard.SelectedNumber > botCard.SelectedNumber)
+        //    {
+        //        var winMessage = new DiscordEmbedBuilder
+        //        {
+        //            Title = "Congrats! You win!",
+        //            Color = DiscordColor.Red
+        //        };
+
+        //        await ctx.Channel.SendMessageAsync(embed: winMessage);
+        //    }
+        //    else
+        //    {
+        //        var loseMessage = new DiscordEmbedBuilder
+        //        {
+        //            Title = "You lost the game....",
+        //            Color = DiscordColor.Yellow
+        //        };
+        //        await ctx.Channel.SendMessageAsync(embed: loseMessage);
+
+        //    }
+
+
+
+        //}
+    }
+}
